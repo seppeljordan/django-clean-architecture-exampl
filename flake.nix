4c35dcd626398603d0fe21a300b3d0b8c5b3df88
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2021-09-08 12:14:48 +0200

    bla

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "A very basic flake";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    let
+      systemDependent = flake-utils.lib.eachDefaultSystem (system:
+        let pkgs = import nixpkgs { inherit system; };
+        in {
+          devShell = pkgs.mkShell {
+            buildInputs = (with pkgs; [ pkgs.python3 ])
+              ++ (with pkgs.python3.pkgs; [ django_3 ]);
+          };
+        });
+      systemIndependent = { };
+    in systemDependent // systemIndependent;
+}
---
+++ todo/use_cases.py
@@ -0,0 +1,51 @@
+from dataclasses import dataclass
+from typing import List
+from abc import ABC, abstractmethod
+
+
+class TodosDatabaseGateway(ABC):
+    @abstractmethod
+    def add_todo(self) -> None:
+        pass
+
+    @abstractmethod
+    def get_all_todos(self) -> List[None]:
+        pass
+
+class AddTodoRequest:
+    pass
+
+
+@dataclass
+class AddTodoUseCase:
+    todos_db_gateway: TodosDatabaseGateway
+
+    def __call__(self, request: AddTodoRequest):
+        self.todos_db_gateway.add_todo()
+
+
+@dataclass
+class ListTodosResponse:
+    todos: List[None]
+    
+
+@dataclass
+class ListTodosUseCase:
+    todos_db_gateway: TodosDatabaseGateway
+
+    def __call__(self):
+        return ListTodosResponse(
+            todos=self.todos_db_gateway.get_all_todos()
+        )
+
+
+class CompleteTodoUseCase:
+    pass
+
+
+class EditTodoUseCase:
+    pass
+
+
+class DeleteTodoUseCase:
+    pass
